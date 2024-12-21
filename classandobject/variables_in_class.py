45eb5839c8f1c03915a103a8cece614dba074628
umaji bagal <bagalumaji@gmail.com>
2024-12-21 10:25:19 +0530

    added class for variables in class

---
+++ classandobject/variables_in_class.py
@@ -0,0 +1,23 @@
+""" 3 types of variables instance, class and local"""
+
+
+class VariableTypes:
+    # class variable
+    SCHOOL_NAME = "BAGAl"
+
+    def __init__(self):
+        self.first_name = 'sayaji'
+        self.last_name = 'bagal'
+
+
+    def print_data(self):
+        city = 'Pandharpur'
+        print("first name {} and last name ",self.first_name,self.last_name)
+        print("city : " , city)
+        print("school name : ",self.SCHOOL_NAME)
+
+
+obj = VariableTypes()
+obj.print_data()
+
+
