a2aa9759ff08f630338dc58cc1de44fce1e5e519
umaji bagal <bagalumaji@gmail.com>
2024-12-21 10:57:46 +0530

    updated

--- classandobject/variables_in_class.py
+++ classandobject/variables_in_class.py
@@ -12,7 +12,7 @@ class VariableTypes:
 
     def print_data(self):
         city = 'Pandharpur'
-        print("first name {} and last name ",self.first_name,self.last_name)
+        print("first name {} and last name ".format(self.first_name,self.last_name))
         print("city : " , city)
         print("school name : ",self.SCHOOL_NAME)
 
