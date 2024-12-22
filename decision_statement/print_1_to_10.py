fee63b2be69197cc2835e29c706a5305f2f5602a
umaji bagal <bagalumaji@gmail.com>
2024-12-22 08:01:53 +0530

    print 1 to 10

---
+++ decision_statement/print_1_to_10.py
@@ -0,0 +1,12 @@
+def print_1_to_10():
+    for i in range(1, 11):
+        print(i)
+
+
+def print_10_to_1():
+    for i in range(10, 0, -1):
+        print(i)
+
+
+# print_1_to_10()
+print_10_to_1()
