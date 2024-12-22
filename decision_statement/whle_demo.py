ceea1d3a07d9e9b03387dd8575be680c3dbd4bd6
umaji bagal <bagalumaji@gmail.com>
2024-12-22 08:02:07 +0530

    while demo

---
+++ decision_statement/whle_demo.py
@@ -0,0 +1,14 @@
+def print_number(num):
+    while num < 10:
+        print(num)
+        num += 1
+
+
+def print_in_descending_order(num):
+    while num >= 1:
+        print(num)
+        num -= 1
+
+
+print_number(1)
+print_in_descending_order(10)
