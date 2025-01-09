fb3c5823dc12eeeaef6b757c03f5831c30ff931d
umaji bagal <bagalumaji@gmail.com>
2025-01-09 17:17:24 +0530

    multiple inheritance

---
+++ inheritance/multipleinheritance2.py
@@ -0,0 +1,19 @@
+class P1:
+    def m(self):
+        print("parent 1 method")
+
+
+class P2:
+    def m(self):
+        print("parent 2 method")
+
+
+class C(P1, P2):
+    def m3(self):
+        print("child method")
+
+
+if __name__ == "__main__":
+    c = C()
+    c.m()
+    c.m3()
