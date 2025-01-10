c343d19b5a2fcba1596a672b3e36e3a906424878
umaji bagal <bagalumaji@gmail.com>
2025-01-10 10:53:11 +0530

    hybrid inheritance

--- inheritance/hybridinheritance.py
+++ inheritance/hybridinheritance.py
@@ -1,18 +1,26 @@
 class P:
-    pass
+    def m1(self):
+        print("Parent method")
 
 
 class C1(P):
-    pass
+    def m2(self):
+        print("Child-1 method")
 
 
 class C2(P):
-    pass
+    def m3(self):
+        print("Child-2 method")
 
 
 class D(C1, C2):
-    pass
+    def m4(self):
+        print("Grand Child method")
 
 
 if __name__ == "__main__":
     d = D()
+    d.m1()
+    d.m2()
+    d.m3()
+    d.m4()
